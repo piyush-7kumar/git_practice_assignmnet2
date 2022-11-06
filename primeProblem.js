1cae88bccad9dca7a0aa764ea780770f8cf7c83e
Piyush Kumar <kumarpiyush913565@gmail.com>
2022-11-06 21:28:47 +0530

    check prime

---
+++ primeProblem.js
@@ -0,0 +1,13 @@
+//check if number is prime or not
+let num = 457;
+let count = 0;
+for (let i = 1; i < num; i++) {
+  if (num % i == 0) {
+    count++;
+  }
+}
+if (count !== 2) {
+  console.log(num, " is Prime number");
+} else {
+  console.log(num, "is not Prime ");
+}
